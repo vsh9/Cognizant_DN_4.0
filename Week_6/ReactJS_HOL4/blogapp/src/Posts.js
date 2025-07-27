8c8eb31b6bff08ecbb1470b615d4b19500f607f7
V Shree Harshitha <vsh9945@gmail.com>
2025-07-28 02:31:10 +0530

    ReactJS_HOL 4 done

---
+++ Week_6/ReactJS_HOL4/blogapp/src/Posts.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import Post from './Post';
+
+class Posts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: [],
+            hasError: false
+        };
+    }
+    
+    loadPosts = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => {
+            const posts = data.map(
+                item => new Post(
+                    item.userId, 
+                    item.id, 
+                    item.title, 
+                    item.body
+                ));
+                this.setState({ posts });
+            })
+        .catch(error => {
+            console.error("Error loading posts", error);
+            this.setState({ hasError: true });
+        });
+    };
+    
+    componentDidMount() {
+        this.loadPosts();
+    }
+
+    componentDidCatch(error, info) {
+        console.error("Caught error:", error, info);
+        alert("Something went wrong while rendering the posts.");
+        this.setState({ hasError: true });
+    }
+
+render() {
+    if (this.state.hasError) {
+        return <h2>Failed to load posts.</h2>;
+    }
+
+    return (
+        <div>
+        <h1>Blog Posts</h1>
+        {this.state.posts.map(post => (
+            <div key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+        <hr />
+        </div>
+    ))}
+    </div>
+    );
+}
+}
+
+export default Posts;
