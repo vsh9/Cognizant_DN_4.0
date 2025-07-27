a523e207c9f996ba0996705d54a300733cefae4a
V Shree Harshitha <vsh9945@gmail.com>
2025-07-28 01:44:25 +0530

    ReactJS_HOL 3 completed

---
+++ Week_6/ReactJS_HOL3/scorecalculatorapp/src/App.js
@@ -0,0 +1,15 @@
+import {CalculateScore} from '../src/Components/CalculateScore';
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name={"Steeve"}
+      School={"DNV Public School"}
+      total={284}
+      goal={3} 
+      />
+    </div>
+  );
+}
+
+export default App;
